6f0a2276a50a1789c05905897f3d5da9f6e196a6
Javier <javieralbertoni@gmail.com>
2022-03-14 17:31:26 -0300

    rutas de carrito terminadas

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,42 +1,74 @@
 const express = require("express");
 const router = express.Router();
-const Cart = require("../models/Cart")
+const Cart = require("../models/Cart");
 const Products = require("../models/Products");
 const User = require("../models/Users");
 
-const Users = require("../models/Users")
+const Users = require("../models/Users");
 
+router.post("/add", async function (req, res) {
+  if (!req.user) return res.send("No se encontró el usuario");
+  const user = await Users.findOne({
+    where: { id: req.user.id },
+    include: { model: Products, as: "products" },
+  });
+  if (!req.body.id) return res.send("No se encontró el producto");
+  const product = await Products.findOne({ where: { id: req.body.id } });
+  if (!product) return res.send("No se encontró el producto");
+  await user.addProducts(product);
+  return res.send("Producto añadido correctamente");
+});
 
-router.post('/add', (req, res) => {
-  Users.findOne({ where: { id: req.user.id }, include: { model: Products, as: "products" } })
-    .then(user => {
-      Products.findOne({ where: { id: req.body.id } })
-        .then(product => {
-          user.addProducts(product)
-          res.send(user);
-        })
-    })
-})
+router.get("/get", async function (req, res) {
+  if (!req.user) return res.send("No se encontró el usuario");
+  const user = await Users.findOne({
+    where: { id: req.user.id },
+    include: { model: Products, as: "products" },
+  });
+  if (!user) return res.send("No se encontró el usuario");
+  return res.send(user.products);
+});
 
-router.delete('/delete/:id', (req, res) => {
-  Cart.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then(() => res.sendStatus(202))
-})
+router.post("/delete", async function (req, res) {
+  if (!req.user) return res.send("No se encontró el usuario");
+  const user = await Users.findOne({
+    where: { id: req.user.id },
+    include: { model: Products, as: "products" },
+  });
+  if (!req.body.id) return res.send("No se encontró el producto");
+  const product = await Products.findOne({ where: { id: req.body.id } });
+  if (!product) return res.send("No se encontró el producto");
+  let bool = false;
+  user.products.map((value) => {
+    if (value.id === req.body.id) bool = true;
+  });
+  if (bool === false)
+    return res.send("No se encontró el producto en el carrito");
+  await user.removeProducts(product);
+  return res.send("Producto eliminado correctamente");
+});
 
-router.put('/edit/:id', (req, res) => {
-  Products.update(req.params.count, {
-    where: {
-      id: req.params.id,
-    },
-    returning: true,
-  })
-    .then(cartProduct => {
-      res.send(cartProduct);
-    })
-})
+router.post("/edit", async function (req, res) {
+  if (!req.user) return res.send("No se encontró el usuario");
+  const user = await Users.findOne({
+    where: { id: req.user.id },
+    include: { model: Products, as: "products" },
+  });
+  if (!req.body.id) return res.send("No se encontró el producto");
+  let product = await Products.findOne({ where: { id: req.body.id } });
+  if (!product) return res.send("No se encontró el producto en el carrito");
+  await product.update({ quantity: req.body.quantity });
+  await product.save();
+  if (!product) return res.send("No se encontró el producto");
+  let bool = false;
+  user.products.map((value) => {
+    if (value.id === req.body.id) bool = true;
+  });
+  if (bool === false)
+    return res.send("No se encontró el producto en el carrito");
+  await user.removeProducts(product);
+  await user.addProducts(product);
+  return res.send("El producto se actualizó correctamente");
+});
 
-module.exports = router
+module.exports = router;
