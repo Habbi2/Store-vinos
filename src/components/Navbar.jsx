10891f4833fa1343163a30bdc14c9e1c3bc94718
JeanLarez <jeanlarez@gmail.com>
2022-03-16 14:43:57 -0300

    boton lista

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,33 @@ const NavbarP = () => {
   };
 
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-expand-lg">
       <div className="titulo">
         <Link to="/">
-          <img className="navbar-brand" src={logoCopa} alt="" style={{width: 50}}/>
+          <img
+            className="navbar-brand"
+            src={logoCopa}
+            alt=""
+            style={{ width: 50 }}
+          />
           Alguien dijo Vino?
         </Link>
       </div>
 
+      <button
+        class="navbar-toggler border border-light"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="bi bi-list fs-1 text-light" ></i>
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
       <div className="productos">
         <Link to="/productos">Productos</Link>
@@ -36,46 +54,45 @@ const NavbarP = () => {
           type="search"
           placeholder="Buscar..."
         />
-    
-      <div class="btn-group">
-        <button
-          type="button"
-          class="btn btn-dark dropdown-toggle boton-despliegue is-info"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          CATEGORIAS
-        </button>
-        <ul class="dropdown-menu">
-          <li>
-            <a class="dropdown-item nohover" href="#">
-              Tinto
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider" />
-          </li>
-          <li>
-            <a class="dropdown-item nohover" href="#">
-              Blanco
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider" />
-          </li>
-          <li>
-            <a class="dropdown-item nohover" href="#">
-              Rosado
-            </a>
-          </li>
-        </ul>
+
+        <div class="btn-group">
+          <button
+            type="button"
+            class="btn btn-dark dropdown-toggle boton-despliegue is-info"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            CATEGORIAS
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item nohover" href="#">
+                Tinto
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item nohover" href="#">
+                Blanco
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item nohover" href="#">
+                Rosado
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      </div>
-      
 
       <div className="carrito">
         <i class="bi bi-cart4 positio-relative">
-          <span class="position-absolute  translate-middle badge rounded-pill bg-success fs-4 size-badge">
+          <span class="position-absolute  translate-middle badge rounded-pill fs-4 size-badge">
             1<span class="visually-hidden">unread messages</span>
           </span>
         </i>
@@ -95,6 +112,7 @@ const NavbarP = () => {
             <Link to="/register">Sing up</Link>
           </div>
         )}
+      </div>
       </div>
     </nav>
   );
